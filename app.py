d748b8872a5c784b70c28b83ce848389e97f1359
lukecassidy8 <132467094+lukecassidy8@users.noreply.github.com>
2024-03-26 11:49:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 def index():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run()
