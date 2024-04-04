54873119527ad1a4e0fd5f5b3a1783cad5b643ca
luke.cassidy <lukemcassidy@gmail.com>
2024-04-04 11:25:44 +0100

    Initialised db connection

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-# app.py
-
 from flask import Flask, render_template, request, jsonify
+from db import createConnection
 
 app = Flask(__name__)
+
+connectionString = "mongodb://waggly-main-users-db:PxfLHSJwuGt9eRWw81Ow5cRFt2Vah60pg44C3nDxZ80J6yUZxbhrwLGhxfTs31uHC64TrI8IL7yJACDbzMYvWw==@waggly-main-users-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@waggly-main-users-db@"
+
+connectionResult = createConnection(connectionString) # Client Object
+# db = createConnection["users"] # DB Object
+# collection_one = db["registry"] # Collection
+
 
 # Routes
 @app.route('/')
